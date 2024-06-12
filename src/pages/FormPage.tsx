987d467571656e16a0a8312b9765caa6dde8dc56
jamesc66 <james@ghomelink.co>
2024-06-12 20:52:02 +0100

    make forms dynamic

--- src/pages/FormPage.tsx
+++ src/pages/FormPage.tsx
@@ -1,4 +1,4 @@
-import { onMount, Component } from "solid-js";
+import { onMount, Component, For } from "solid-js";
 import { useData } from "../contexts/store";
 import { Form } from "../components/Form";
 
@@ -19,33 +19,20 @@ export const FormPage: Component = () => {
         <p>Loading...</p>
       ) : (
         <>
-          <h1>Users Form</h1>
-          <Form
-            fields={forms().users}
-            data={data().users[0]}
-            onSubmit={handleSubmit}
-          />
-
-          <h1>Brands Form</h1>
-          <Form
-            fields={forms().brands}
-            data={data().brands[0]}
-            onSubmit={handleSubmit}
-          />
-
-          <h1>Products Form</h1>
-          <Form
-            fields={forms().products}
-            data={data().products[0]}
-            onSubmit={handleSubmit}
-          />
-
-          <h1>Reviews Form</h1>
-          <Form
-            fields={forms().reviews}
-            data={data().reviews[0]}
-            onSubmit={handleSubmit}
-          />
+          <For each={forms()}>
+            {(form) => (
+              <div key={form.id}>
+                <h1>
+                  {form.id.charAt(0).toUpperCase() + form.id.slice(1)} Form
+                </h1>
+                <Form
+                  fields={form.data}
+                  data={data()?.[form.id]?.[0]}
+                  onSubmit={handleSubmit}
+                />
+              </div>
+            )}
+          </For>
         </>
       )}
     </div>
